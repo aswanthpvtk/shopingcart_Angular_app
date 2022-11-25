aadfe874bcf4572a1ab62bda6a8729c34d1a11f3
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-25 14:49:12 +0530

    shoping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProComponent } from './view-pro/view-pro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    ViewProComponent
   ],
   imports: [
     BrowserModule,
